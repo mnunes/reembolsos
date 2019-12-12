7cec8b02c77fdd8e1dcf8beacda4234bc8823797
Marcus Nunes <marcus.nunes@gmail.com>
2019-12-12 10:24:47 -0300

    Bug na identificação do folder corrigido

--- R/completar.R
+++ R/completar.R
@@ -1,6 +1,10 @@
 completar <- function(){
 
-  folder <- paste(.libPaths(), "/reembolsos", sep="")
+  pacotes <- installed.packages()
+
+  index <- which(rownames(pacotes) == "reembolsos")
+
+  folder <- paste(pacotes[index, 2], "/reembolsos", sep="")
 
   download.file("https://marcusnunes.me/data/Rdata.rdb",
                 destfile = paste(folder, "/Rdata.rdb", sep = ""))
