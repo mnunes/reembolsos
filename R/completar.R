0618173b450f8c73acd7e637635c7291839bea3c
Marcus Nunes <marcus.nunes@gmail.com>
2019-12-16 08:55:31 -0300

    Typos corrigidos

--- R/completar.R
+++ R/completar.R
@@ -9,8 +9,8 @@ completar <- function(){
   download.file("https://marcusnunes.me/data/Rdata.rdb",
                 destfile = paste(folder, "/Rdata.rdb", sep = ""))
   download.file("https://marcusnunes.me/data/Rdata.rds",
-                destfile = paste(folder, "/Rdata.rdb", sep = ""))
+                destfile = paste(folder, "/Rdata.rds", sep = ""))
   download.file("https://marcusnunes.me/data/Rdata.rdx",
-                destfile = paste(folder, "/Rdata.rdb", sep = ""))
+                destfile = paste(folder, "/Rdata.rdx", sep = ""))
 
 }
